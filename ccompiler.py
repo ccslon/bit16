9f14d02cce05baf18e9934eda2bc20c81d017c0a
ccslon <ccs1school@gmail.com>
2023-10-25 17:31:05 -0400

    Changed flag

--- ccompiler.py
+++ ccompiler.py
@@ -8,7 +8,7 @@ Created on Thu Aug 31 21:52:15 2023
 import assembler
 import cparser
 
-def ccompile(file_name, sflag=False, oflag=True):
+def ccompile(file_name, sflag=False, fflag=True):
     if file_name.endswith('.c') or file_name.endswith('.h'):
         with open(file_name) as in_file:
             text = in_file.read()
@@ -16,17 +16,17 @@ def ccompile(file_name, sflag=False, oflag=True):
     asm = ast.compile()
     if sflag:
         print(asm)
-        if oflag:
+        if fflag:
             with open(f'{file_name[:-2]}.s', 'w+') as out_file:
                 out_file.write(asm)
     else:
         bit16 = assembler.assemble(asm)
-        if oflag:
+        if fflag:
             with open(f'{file_name[:-2]}.bit16', 'w+') as out_file:
                 out_file.write(' '.join(bit16))
 
 if __name__ == '__main__':
     # ccompile('hello.c', sflag=True, oflag=False)    
-    # ccompile('tests//globs.c', sflag=True, oflag=False)
-    ccompile('tests//globs.c')
+    ccompile('tests//structs.c', sflag=True, fflag=False)
+    # ccompile('tests//globs.c')
     # ccompile('std//stdio.h', sflag=True, oflag=False)
