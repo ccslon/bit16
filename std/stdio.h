1d7a9e88846986726704e2701fe468e20d1458dc
ccslon <ccs1school@gmail.com>
2023-10-27 16:02:43 -0400

    Slowly implementing

--- std/stdio.h
+++ std/stdio.h
@@ -4,20 +4,26 @@ struct FILE {
     int read;
     int write;
 };
-struct FILE* stdout = {0x7f00, 0, 0};
+
+struct FILE stdout = {0x7f00, 0, 0};
 
 int putchar(char c) {
     fputc(c, stdout);
     return 0;
 }
 int puts(const char* str) {
-    while (*str != '\0') {
-        putchar(*str);
-        str++;
-    }
+    fputs(str, stdout);
     putchar('\n');
     return 0;
 }
+int fputs(const char* str, struct FILE* stream) {
+    while (*str != '\0') {
+        fputc(*str, stream);
+        str++;
+    }
+    return 0;
+}
+
 int fputc(char c, struct FILE* stream) {
     stream->buffer[stream->write++] = c;
     return 0;
