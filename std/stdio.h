ce188ae6921060494429d939abced3a8d2cdbb10
ccslon <143284758+ccslon@users.noreply.github.com>
2023-10-27 11:03:49 -0400

    Testing

--- std/stdio.h
+++ std/stdio.h
@@ -1,11 +1,13 @@
-struct FILE {
-    void* buffer;
-    
-}
 
-const char* STDOUT = 0x7fff;
+struct FILE {
+    int* buffer;
+    int read;
+    int write;
+};
+struct FILE* stdout = {0x7f00, 0, 0};
+
 int putchar(char c) {
-    *STDOUT = c;
+    fputc(c, stdout);
     return 0;
 }
 int puts(const char* str) {
@@ -14,5 +16,9 @@ int puts(const char* str) {
         str++;
     }
     putchar('\n');
+    return 0;
+}
+int fputc(char c, struct FILE* stream) {
+    stream->buffer[stream->write++] = c;
     return 0;
 }
--- tests/hello.c
+++ tests/hello.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 void main() {
-    puts("Hello world!");
+    fputc('c', stdout);
 }
