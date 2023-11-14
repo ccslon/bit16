395ccaf990d5eb0d9acd6109aa594b30a00dea12
ccslon <143284758+ccslon@users.noreply.github.com>
2023-11-14 14:22:57 -0500

    Updated stdio

--- std/stdio.h
+++ std/stdio.h
@@ -7,13 +7,12 @@ struct FILE {
 
 struct FILE stdout = {0x7f00, 0, 0};
 
-int putchar(char c) {
-    fputc(c, stdout);
+int fputc(char c, struct FILE* stream) {
+    stream->buffer[stream->write++] = c;
     return 0;
 }
-int puts(const char* str) {
-    fputs(str, stdout);
-    putchar('\n');
+int putchar(char c) {
+    fputc(c, &stdout);
     return 0;
 }
 int fputs(const char* str, struct FILE* stream) {
@@ -23,8 +22,8 @@ int fputs(const char* str, struct FILE* stream) {
     }
     return 0;
 }
-
-int fputc(char c, struct FILE* stream) {
-    stream->buffer[stream->write++] = c;
+int puts(const char* str) {
+    fputs(str, &stdout);
+    putchar('\n');
     return 0;
 }
