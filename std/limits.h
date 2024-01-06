cdd61176b8d80213b80d177949439e44658e5665
ccslon <143284758+ccslon@users.noreply.github.com>
2024-01-06 01:45:44 -0500

    Update limits.h

--- std/limits.h
+++ std/limits.h
@@ -1,48 +1,6 @@
-int strlen(char* str) {
-    int len = 0;
-    while (*str != '\0') {
-        str++;
-        len++;
-    }
-    return len;
-}
-char* strcat(const char* s, const char* t) {
-    int i; int j;
-    i = j = 0;
-    while (s[i] != '\0') 
-        i++;
-    while((s[i++] = t[j++]) != '\0')
-        ;
-}
-char* strrev(char* str) {
-    int front = 0;
-    int back = strlen(str) - 1;
-    while (front < back) {
-        int temp = str[front];
-        str[front] = str[back];
-        str[back] = temp;
-        front++;
-        back--;
-    }
-    return str;
-}
-int strcmp(char* str1, char* str2) {
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    if (len1 != len2) return len1 - len2;
-    int i;
-    for (i = 0; i < len1; i++) {
-        if (str1[i] != str2[i]) return str1[i] - str2[i];
-    }
-    return 0;
-}
-int strncmp(char* str1, char* str2, int n) {
-    int len1 = strlen(str1);
-    int len2 = strlen(str2);
-    if (len1 != len2) return len1 - len2;
-    int i;
-    for (i = 0; i < len1 && i < n; i++) {
-        if (str1[i] != str2[i]) return str1[i] - str2[i];
-    }
-    return 0;
-}
+const int CHAR_BIT = 16;
+const int CHAR_MAX = 65535;
+const int CHAR_MIN = 0;
+const int INT_MAX = 32767;
+//const int INT_MIN = -32768;
+const int UINT_MAX = 65535;
