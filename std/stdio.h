9eaf2b110033c0a60f8c44e09a79a7d12a094e0b
ccslon <143284758+ccslon@users.noreply.github.com>
2024-01-06 01:36:19 -0500

    Added printf!

--- std/stdio.h
+++ std/stdio.h
@@ -25,3 +25,41 @@ int puts(const char* str) {
     putchar('\n');
     return 0;
 }
+void printd(int n) {
+    if (n < 0) {
+        putchar('-');
+        n = -n;
+    }
+    int div = 0;
+    int mod = n;
+    while (mod >= 10) { // "division"
+        mod -= 10;
+        div++;
+    }
+    if (div) 
+        printd(div);
+    putchar(mod + '0');
+}
+void printf(char* fmt, ...) {
+    int* ap;
+    (ap = _VARLIST_);
+    char* c;
+    for (c = fmt; *c; c++) {
+        if (*c == '%') {
+            switch (*++c) {
+                case 'd': {
+                    printd(((int)*ap++));
+                    break;
+                }
+                case 's': {
+                    fputs(((char*)*ap++), &stdout);
+                    break;
+                }
+                default: putchar(*c);
+            }
+        } else {
+            putchar(*c);
+        }
+    }
+    (ap = 0);
+}
