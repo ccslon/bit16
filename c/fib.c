837f5d098f1a1fc4832e15956555fd51b8ba876e
ccslon <ccs1school@gmail.com>
2024-01-25 16:27:14 -0500

    Some changes

--- c/fib.c
+++ c/fib.c
@@ -22,7 +22,7 @@ int tailfib(const int n) {
 }
 void main() {
     int i;
-    printf("Fib!\n");
+    printf("Fib! %d %d\n", 0, 1);
     for (i = 1; i <= 10; i++)
         printf("%d\n", tailfib(i));    
 }
